800f635df262c45eb8432dbf3c2d395ef6ca9734
Lauren <halpertln@gmail.com>
2023-01-17 12:08:02 -0700

    added a picture to about me

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ const h2 = document.createElement("h2");
 h2.textContent = "My Favorites";
 document.querySelector("div#favorites").appendChild(h2);
 
-const ul = document.getElementsByClassName("books-movies-food");
-document.querySelector("div#favorites").appendChild(ul);
+
