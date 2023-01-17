cd5762288b5182b20eabead9286fa4de0b4e2c57
Lauren <halpertln@gmail.com>
2023-01-17 14:47:25 -0700

    added js events to me and travel

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const h2 = document.createElement("h2");
 h2.textContent = "My Favorites";
 document.querySelector("div#favorites").appendChild(h2);
 
+const me = document.getElementById("me");
+function clickAlert () {
+    alert("It's me, hi!");
+}
+me.addEventListener("click", clickAlert);
 
+const travel = document.getElementById("travel");
+function clickAlert2 () {
+    alert("I like to travel to cool places.");
+}
+travel.addEventListener("dblclick", clickAlert2);
